0d9a31a400a2ba7c5de60103589a8b5d1060a713
Rheno001 <renfredreuben@gmail.com>
2024-03-20 14:57:44 +0100

    Javascript started

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**adding event listener */
+
+const addEventOnElements = function(elements, eventType, callback){
+    for (let i = 0, len = elements.length; i < len; i++){
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+/**Navbar toggle for mobile */
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function (){
+    navbar.classList.toggle("active");
+    navbar.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
