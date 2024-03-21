f14fcad1394c680b4b9569c7edc4b606c6803247
Rheno001 <renfredreuben@gmail.com>
2024-03-21 01:11:40 +0100

    hero section

--- script.js
+++ script.js
@@ -15,8 +15,21 @@ const overlay = document.querySelector("[data-overlay]");
 
 const toggleNavbar = function (){
     navbar.classList.toggle("active");
-    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
     document.body.classList.toggle("nav-active");
 }
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
+
+/**
+ * active header when window scroll down
+ */
+const header = document.querySelector("[data-header");
+
+window.addEventListener("scroll", function() {
+    if (this.window.scrollY > 100){
+        header.classList.add("active");
+    } else{
+        header.classList.remove("active");
+    }
+})
